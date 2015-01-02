be2a98cd0a7252380ccd9c856ddb73bc4aa97ef6
Emilien Damart <edamart@axome.com>
2015-01-02 18:22:39 +0100

    [*] Intégration Flash

--- js/demo.js
+++ js/demo.js
@@ -3,6 +3,9 @@ var canvas = document.getElementById("canvas");
 var video = null;
 var rafId = 0;
 var frames = [];
+var flash_interval = 0;
+var flash_image_pos = 0;
+var flash_image = [];
 
 var options = {
 	"audio": false, //false: OTHERWISE FF nightlxy throws an NOT IMPLEMENTED error
@@ -19,8 +22,16 @@ var options = {
 
 	mode: "callback",
 	// callback | save | stream
-	swffile: "../dist/fallback/jscam_canvas_only.swf",
-	quality: 85
+	swffile: "swf/jscam_canvas_only.swf",
+	quality: 85,
+	onSave: flashCapture,
+	onCapture: function()
+	{
+		window.webcam.save();
+	},
+	onTick: function(timer)
+	{
+	}
 }
 
 function onSuccess(stream)
@@ -51,41 +62,62 @@ function onSuccess(stream)
 
 	} else{
 		// flash context
+		console.log(window.webcam);
 	}
 }
 
 function onError(error)
 {
 	alert('No camera available.');
-	console.error('An error occurred: [CODE ' + error.code + ']');
+	
+	if (typeof(error.code) != 'undefined')
+		console.error('An error occurred: [CODE ' + error.code + ']');
 }
 
 function startRecording()
 {
 	isRecording = true;
 	$('#captureVideo').text('Stop capture');
- 
-	canvas.width = video.videoWidth;
-	canvas.height = video.videoHeight;
-	canvas.getContext('2d').drawImage(video, 0, 0);
+	
+	if (options.context == 'webrtc')
+	{
+		canvas.width = video.videoWidth;
+		canvas.height = video.videoHeight;
+		canvas.getContext('2d').drawImage(video, 0, 0);
 
-	var url = canvas.toDataURL('image/webp', 1);
-	video = video;
-
-	rafId = window.requestAnimationFrame(drawVideoFrame);
+		var url = canvas.toDataURL('image/webp', 1);
+		
+		rafId = window.requestAnimationFrame(drawVideoFrame);
+	}
+	else
+	{
+		// Flash
+		window.webcam = this.options;
+		window.webcam.capture();
+	}
 }
 
 function stopRecording()
 {
 	isRecording = false;
 	$('#captureVideo').text('3. Capture video');
-
-	var webmBlob = Whammy.fromImageArray(frames, 1000 / 60);
-	//window.location.href = window.URL.createObjectURL(webmBlob);
+	
+	if (options.context == 'webrtc')
+	{
+		var webmBlob = Whammy.fromImageArray(frames, 1000 / 60);
+		var fileType = 'video';
+		//window.location.href = window.URL.createObjectURL(webmBlob);
+	}
+	else
+	{
+		var webmBlob = frames.join('|');
+		var fileType = 'images';
+	}
 
 	var fd = new FormData();
 	fd.append('fname', 'video.webm');
 	fd.append('data', webmBlob);
+	fd.append('ftype', fileType);
 	$.ajax({
 	    type: 'POST',
 	    url: 'upload.php',
@@ -104,6 +136,20 @@ function drawVideoFrame(time)
 	frames.push(canvas.toDataURL('image/webp', 1));
 }
 
+// FLASH
+function flashCapture(data)
+{
+	flash_image.push(data);
+	
+	flash_image_pos += 4 * 320;
+	
+	if (flash_image_pos >= 4 * 320 * 240) {
+		frames.push(flash_image.join('-----'));
+		flash_image = [];
+		flash_image_pos = 0;
+	}
+}
+
 
 /////////////////////////////////////////////////////////////////////////////////
 // INITIALISATION
@@ -115,19 +161,24 @@ getUserMedia(options, onSuccess, onError);
 
 $('#captureVideo').on('click', function()
 {
+	if (isRecording)
+		stopRecording();
+	else
+		startRecording();
+	
+	/*
 	switch(options.context)
 	{
 		case 'webrtc':
-			if (isRecording)
-				stopRecording();
-			else
-				startRecording();
+			
 		break;
 
 		case 'flash':
+			
 		break;
 
 		default:
 			alert('No context was supplied to getSnapshot()');
 	}
+	*/
 });
--- upload.php
+++ upload.php
@@ -1,5 +1,45 @@
 <?php
 
-if (isset($_FILES["data"])) {
-	move_uploaded_file($_FILES["data"]["tmp_name"], 'video.webm');
+if (!empty($_POST['ftype']))
+{
+	switch($_POST['ftype'])
+	{
+		case 'video':
+			if (isset($_FILES["data"])) {
+				move_uploaded_file($_FILES["data"]["tmp_name"], 'video.webm');
+			}
+		break;
+		
+		case 'images':
+		
+			if (!empty($_POST['data']))
+			{
+				$im = imagecreatetruecolor(320, 240);
+				$i = 0;
+				
+				foreach (explode('|', str_replace("\n", '', $_POST['data'])) as $image)
+				{
+					if (!empty($image))
+					{
+						foreach(explode('-----', $image) as $y => $csv)
+						{
+							$csv = trim($csv);
+							
+							if (!empty($csv))
+							{
+								foreach (explode(";", $csv) as $x => $color) {
+									if (!empty($color))
+										imagesetpixel($im, $x, $y, $color);
+								}
+							}
+						}
+						
+						$i++;
+						
+						imagejpeg($im, 'img-'.$i.'.jpg');
+					}
+				}
+			}
+		break;
+	}
 }
